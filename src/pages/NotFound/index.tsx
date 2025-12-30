391b868d28bc691a5833b888f09c9e0c1de11418
Lucas Andrade <eudevlucas@gmail.com>
2025-12-30 15:23:58 -0300

    ci: fix SPA routes

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,3 +1,4 @@
+import {Link} from "react-router-dom";
 
 function NotFound() {
     return (
@@ -6,9 +7,9 @@ function NotFound() {
                 <h1 className="mb-4 text-4xl font-bold">404</h1>
                 <p className="mb-4 text-xl opacity-75">Oops! Página não encontrada</p>
 
-                <a href="/" className="text-gold no-underline hover:underline">
+                <Link to="/" className="text-gold no-underline hover:underline">
                     Voltar para a página inicial
-                </a>
+                </Link>
             </div>
         </div>
     );
